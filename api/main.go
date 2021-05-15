8c1c98c93bc1f86dee71696ab782a1ad94c1ea7d
yeguacelestial <yeguacelestial@gmail.com>
2021-05-14 20:10:12 -0500

    Create basic login structure

--- api/main.go
+++ api/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type User struct {
+	Email    string `json:"Email"`
+	Password string `json:"Password"`
+}
+
+// Global Users slice. Simulates a database.
+var Users []User
+
+func main() {
+
+	// Create User
+	defaultUser := User{
+		Email:    "demo@usuario.com",
+		Password: "pipjY7-guknaq-nancex",
+	}
+
+	Users = append(Users, defaultUser)
+
+	router := Router()
+	log.Fatal(http.ListenAndServe(":10000", router))
+}
+
 func Router() *mux.Router {
-	router := mux.NewRouter()
+	router := mux.NewRouter().StrictSlash(true)
+
 	router.HandleFunc("/", RootEndpoint).Methods("GET")
+	router.HandleFunc("/login", LoginEndpoint).Methods("POST")
+
 	return router
 }
 
@@ -18,7 +46,30 @@ func RootEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Write([]byte("Hello World"))
 }
 
-func main() {
-	router := Router()
-	log.Fatal(http.ListenAndServe(":12345", router))
+func LoginEndpoint(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	// Unmarshal this into new User struct
+	var user User
+	json.Unmarshal(reqBody, &user)
+
+	k, found := Find(Users, user)
+
+	if !found {
+		fmt.Printf("[-] Invalid User or password.\n")
+	} else {
+		fmt.Printf("[+] User with email %s is registered in the db. (Index: %d)\n", Users[k].Email, k)
+	}
+}
+
+// Verify if user is registered in the database (slice)
+func Find(slice []User, val User) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+
+	return -1, false
 }
--- api/main_test.go
+++ api/main_test.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	userEmail string = "demo@usuario.com"
+	password  string = "pipjY7-guknaq-nancex"
+)
+
+func handleErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func TestRootEndpoint(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/", nil)
@@ -16,4 +30,23 @@ func TestRootEndpoint(t *testing.T) {
 
 	assert.Equal(t, 200, response.Code, "OK Response is expected")
 	assert.Equal(t, "Hello World", response.Body.String(), "Incorrect body found")
+}
+
+// Login with default email & password
+func TestLoginEndpoint(t *testing.T) {
+	values := map[string]string{
+		"userEmail": userEmail,
+		"password":  password,
+	}
+	json_data, err := json.Marshal(values)
+	handleErr(err)
+
+	resp, err := http.NewRequest("POST", "/login", bytes.NewBuffer(json_data))
+	handleErr(err)
+
+	var res map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&res)
+
+	assert.Equal(t, "", res["json"], "TODO: Fix test and create endpoint")
 }
